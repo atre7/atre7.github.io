bc2e03dfa7f5acf2412b103440aa5dcedd4db233
atre7 <atrerc@gmail.com>
2015-08-24 00:37:15 +0200

    atom

--- navbarApp.js
+++ navbarApp.js
@@ -1,40 +1,41 @@
 
 
-(function ($) {
-  $(document).ready(function(){
-    console.log("window height " +$(window).height());
-    console.log("doc height "+ $(document).height());
+
+(function($) {
+  $(document).ready(function() {
+    console.log("window height " + $(window).height());
+    console.log("doc height " + $(document).height());
     console.log("screen height " + screen.height);
-	
-if($(window).height() < 800){
-	// hide .navbar first
-	$(".navbar").hide();
-	
-	// fade in .navbar
-	$(function () {
 
-		$(window).scroll(function () {
-            // set distance user needs to scroll before we fadeIn navbar
-			console.log("srollTop : "+$(this).scrollTop());
-			if ($(this).scrollTop() < 100) {
-				$('.navbar').fadeIn();
-			} else {
-				$('.navbar').fadeOut();
-			}
-		});
-	});
-}
-else{
-	$('.navbar').fadeIn();
-}
+    if ($(window).height() < 800) {
+      // hide .navbar first
+      $(".navbar").hide();
 
-$('.collapse a').click(function(){
-	console.log("klikol na a ");
-	//$('.navbar').fadeOut();
-	//$('#menuBtn').removeClass("navbar-toggle");
-	$('#myNvb').collapse('hide');
-	// $('.navbar-fixed-top').autoHiddingNavbar()  
-});
+      // fade in .navbar
+      $(function() {
 
-});
-  }(jQuery));
+        $(window).scroll(function() {
+          // set distance user needs to scroll before we fadeIn navbar
+          console.log("srollTop : " + $(this).scrollTop());
+          if ($(this).scrollTop() < 100) {
+            $('.navbar').fadeIn();
+          } else {
+            $('.navbar').fadeOut();
+          }
+        });
+      });
+    } else {
+      $('.navbar').fadeIn();
+    }
+
+
+    $('.collapse a').click(function() {
+      console.log("klikol na a ");
+      //$('.navbar').fadeOut();
+      //$('#menuBtn').removeClass("navbar-toggle");
+      $('#myNvb').collapse('hide');
+      // $('.navbar-fixed-top').autoHiddingNavbar()
+    });
+
+  });
+}(jQuery));
