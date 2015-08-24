586354b5c4458908dc81ea4da83836dee9d0ae2e
atre7 <atrerc@gmail.com>
2015-08-24 12:54:16 +0200

    log w/h on screen

--- navbarApp.js
+++ navbarApp.js
@@ -32,7 +32,10 @@
     console.log("window height " + $(window).height());
     console.log("doc height " + $(document).height());
     console.log("screen height " + screen.height);
-
+    $('#pWidthWindow').text("windov width : " + $(window).width());
+    $('#pHeightWindow').text("window height : " + $(window).height());
+    $('#pWidthScreen').text("screen width : " + screen.width);
+    $('#pHeightSreen').text("screen height : " + screen.height);
     if ($(window).height() < 800) {
       // hide .navbar first
       $(".navbar").hide();
