6a74d70dae2a5bc5aa0811006850a3f79fd049ce
atre7 <atrerc@gmail.com>
2015-08-25 17:05:57 +0200

    getPos > watchPos

--- navbarApp.js
+++ navbarApp.js
@@ -24,7 +24,7 @@
 
       function getLocation() {
         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition);
+          navigator.geolocation.watchPosition(showPosition);
         } else {
           x.innerHTML = "Geolocation is not supported by this browser.";
         }
